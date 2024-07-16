6604679eb02fba2705ead5546b677710ef5cc568
yunsteel <archer0082@naver.com>
2024-07-16 14:29:00 +0900

    Tanstack query 설치하기

--- lib/supabase/queries/knaji/select.ts
+++ lib/supabase/queries/knaji/select.ts
@@ -1,8 +1,16 @@
+import { DEFAULT_SIZE, PageParams } from '@/types/Pagination'
 import { supabase } from '../..'
 import { Grade } from '../../type'
 
 const grades: Grade[] = ['1', '2', '3', '4', '5', '6', 'S']
 
-export const getKanjiListByGrade = async (grade: (typeof grades)[number]) => {
-  return supabase.from('상용한자').select('*').eq('학년', grade)
+export const getKanjiListByGrade = async (
+  grade: (typeof grades)[number],
+  { page = 0, size = DEFAULT_SIZE }: PageParams,
+) => {
+  return supabase
+    .from('상용한자')
+    .select('*')
+    .eq('학년', grade)
+    .range(page * size, (page + 1) * size - 1)
 }
